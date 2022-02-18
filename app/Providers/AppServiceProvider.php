874cd269cad453e9fe06566adbbaf705b788d8e1
yousef <yousef0sarkar@gmail.com>
2022-02-19 00:51:30 +0300

    just start

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\SallaAuthService;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+        $this->app->singleton('salla.auth', function () {
+            return $this->app->make(SallaAuthService::class);
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        Schema::defaultStringLength(191);
+    }
+}
