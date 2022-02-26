7d919c9081c0f18b5f57489cf361bd06304142ad
yousef <yousef0sarkar@gmail.com>
2022-02-27 01:44:04 +0300

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -57,10 +57,9 @@ class DashboardController extends Controller
                 $products = $this->salla->request('GET', 'https://api.salla.dev/admin/v2/products')['data'];
                  $orders = $this->salla->request('GET', 'https://api.salla.dev/admin/v2/orders?page = 1 ')['data'];
                  $orders2 = $this->salla->request('GET', 'https://api.salla.dev/admin/v2/orders?page = 2 ')['data'];
-                $test = json_encode($orders);
                  $order3 =   $orders+  $orders2;
 
-                return response()->json($test
+                return response()->json(['orders'=>$orders]
                 );
              
             //    $data = $this->salla->request('GET', 'https://accounts.salla.sa/oauth2/user/info')['data'];
