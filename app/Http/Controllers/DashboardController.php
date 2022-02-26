08347a8c82adbed470c17854e6f302c135a0caa5
yousef <yousef0sarkar@gmail.com>
2022-02-26 23:59:58 +0300

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -54,13 +54,13 @@ class DashboardController extends Controller
             $store = $this->salla->getStoreDetail();
 
             // let's get the product of store via salla service
-               $products = $this->salla->request('GET', 'https://api.salla.dev/admin/v2/products')['data'];
+           return     $products = $this->salla->request('GET', 'https://api.salla.dev/admin/v2/products')['data'];
                 $orders = $this->salla->request('GET', 'https://api.salla.dev/admin/v2/orders?page');
                $data = $this->salla->request('GET', 'https://accounts.salla.sa/oauth2/user/info')['data'];
             //   $categroes = $this->salla->request('GET', 'https://api.salla.dev/admin/v2/categoriess')['data'];
             // return   $customers = $this->salla->request('GET', 'https://api.salla.dev/admin/v2 /customers');
             return response()->json([
-                'order' => $data
+                'order' => $orders
               
             ]);
                  
