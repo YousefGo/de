c7e442f110e2661fde9047eab884386292afde4e
yousef <yousef0sarkar@gmail.com>
2022-02-27 06:06:55 +0300

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,9 +7,8 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth as outh;
 use App\Models\OauthToken;
-
 
 class LoginController extends Controller
 {
@@ -46,7 +45,7 @@ class LoginController extends Controller
     public function logout(Request $request)
     {
         $this->guard()->logout();
-        OauthToken::where('user_id',Auth::user()->id)->delete();
+        OauthToken::where('user_id',outh::user()->id)->delete();
 
         $request->session()->invalidate();
 
